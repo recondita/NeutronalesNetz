3aca831b1068aa9a0ea71675201a16f467a92c21
Richard Polzer <richard@polzer-eb.de>
2014-03-08 19:42:36 +0100

    Loader erweitert

--- NeuronalesNetz/src/Loader.java
+++ NeuronalesNetz/src/Loader.java
@@ -1,8 +1,19 @@
+import org.neuroph.core.NeuralNetwork;
 
 public class Loader {
 
 	public Loader() {
 		// TODO Auto-generated constructor stub
+
+	}
+
+	public void save(NeuralNetwork neuralNetwork, String destination) {
+		neuralNetwork.save(destination);
+	}
+
+	public NeuralNetwork load(String file) {
+		NeuralNetwork neuralNetwork = NeuralNetwork.load(file);
+		return neuralNetwork;
 	}
 
 }
