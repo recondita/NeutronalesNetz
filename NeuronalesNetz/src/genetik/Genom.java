f6ec658615708ba187f26147337d6c8d5ed56fe4
jan <jan.hofmeier@yahoo.de>
2014-05-12 16:30:20 +0200

    genom

---
+++ NeuronalesNetz/src/genetik/Genom.java
@@ -0,0 +1,23 @@
+package genetik;
+
+import java.util.List;
+
+import org.neuroph.util.TransferFunctionType;
+
+public class Genom
+{
+
+	private List<Integer> layers;
+	private double learningRate;
+	private double momentum;
+	private long learningTime;
+	private TransferFunctionType transferFunktion;
+	private int dataSet;
+	
+	
+	public Genom()
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+}
