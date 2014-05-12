f5eb11110226deb4a2e2f4e79d6bcb405b505402
jan <jan.hofmeier@yahoo.de>
2014-05-12 22:34:29 +0200

    FastSnake

---
+++ NeuronalesNetz/src/snakeCOM/FastSnake.java
@@ -0,0 +1,20 @@
+package snakeCOM;
+
+import genetik.FitnessTester;
+
+public class FastSnake extends LogSnake
+{
+
+	public FastSnake(int x, int y, int richtung, LogBrett brett,FitnessTester fitnessTester, boolean wachsen)
+	{
+		super(x,y,richtung,1L,brett,fitnessTester,wachsen);
+		brett.disableSuperVerloren();
+	}
+
+	@Override
+	public void start()
+	{
+		while(!fPause&&schritt());
+	}
+	
+}
