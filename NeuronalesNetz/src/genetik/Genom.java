494629e3f70f4106d4748a8a4c9e5ba162c39d82
Richard Polzer <richard@polzer-eb.de>
2014-05-12 19:38:51 +0200

    Getter

--- NeuronalesNetz/src/genetik/Genom.java
+++ NeuronalesNetz/src/genetik/Genom.java
@@ -1,6 +1,8 @@
 package genetik;
 
 import java.util.List;
+import java.lang.Object;
+
 
 import org.neuroph.util.TransferFunctionType;
 
@@ -18,6 +20,36 @@ public class Genom
 	public Genom()
 	{
 		// TODO Auto-generated constructor stub
+	}
+
+
+	public List<Integer> getLayers() {
+		return (List<Integer>) layers.clone();
+	}
+
+
+	public double getLearningRate() {
+		return learningRate;
+	}
+
+
+	public double getMomentum() {
+		return momentum;
+	}
+
+
+	public long getLearningTime() {
+		return learningTime;
+	}
+
+
+	public TransferFunctionType getTransferFunktion() {
+		return transferFunktion;
+	}
+
+
+	public int getDataSet() {
+		return dataSet;
 	}
 
 }
