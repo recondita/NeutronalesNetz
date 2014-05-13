6900b8158496bd28ed93d0510892df82a754a0da
jan <jan.hofmeier@yahoo.de>
2014-05-13 11:41:52 +0200

    guii aus

--- NeuronalesNetz/src/snakeCOM/SnakeControl.java
+++ NeuronalesNetz/src/snakeCOM/SnakeControl.java
@@ -8,25 +8,34 @@ public abstract class SnakeControl
 	protected LogBrett brett;
 	protected LogSnake snake;
 	protected GUI gui;
-	private static boolean wachsen=false;
-	
+	private static boolean wachsen = false;
+
 	public SnakeControl()
 	{
+		this(true);
+	}
+
+	public SnakeControl(boolean gui)
+	{
 		this.brett = new LogBrett(this);
-		gui = new GUI(this.brett);
+		if (gui)
+			this.gui = new GUI(this.brett);
 	}
 
 	public abstract void preMove();
-	
+
 	public LogSnake newSnake()
 	{
 		this.snake = new LogSnake(brett.getBreite() / 2, brett.getHoehe() / 2,
-				1, 500L, this.brett, this,wachsen);
+				1, 500L, this.brett, this, wachsen);
 		return snake;
 	}
-	
+
+	public void start()
+	{
+		brett.start();
+	}
 	
 	public abstract void verloren(int laenge);
-	
 
 }
