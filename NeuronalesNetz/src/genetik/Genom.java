4df3c0298212782bb95607fc970f4cab2d1d3248
Richard Polzer <richard@polzer-eb.de>
2014-05-17 20:56:19 +0200

    neuer rekombi algo

--- NeuronalesNetz/src/genetik/Genom.java
+++ NeuronalesNetz/src/genetik/Genom.java
@@ -7,8 +7,7 @@ import org.neuroph.core.data.DataSet;
 import org.neuroph.util.TrainingSetImport;
 import org.neuroph.util.TransferFunctionType;
 
-public class Genom implements Comparable<Genom>
-{
+public class Genom implements Comparable<Genom> {
 	/**
 	 * [0] muss zum feld passen und [length-1] muss 2 sein!
 	 */
@@ -20,105 +19,131 @@ public class Genom implements Comparable<Genom>
 	private DataSet dataSet;
 	private int fitness;
 
-	public Genom(int[] layers, double learningRate, double momentum, int maxIterations, TransferFunctionType transferFunktion, DataSet trainingSet)
-	{
-		this.layers=layers;
-		this.learningRate=learningRate;
-		this.momentum=momentum;
-		this.maxIterations=maxIterations;
-		this.transferFunktion=transferFunktion;
-		this.dataSet=trainingSet;
+	public Genom(int[] layers, double learningRate, double momentum,
+			int maxIterations, TransferFunctionType transferFunktion,
+			DataSet trainingSet) {
+		this.layers = layers;
+		this.learningRate = learningRate;
+		this.momentum = momentum;
+		this.maxIterations = maxIterations;
+		this.transferFunktion = transferFunktion;
+		this.dataSet = trainingSet;
 	}
-	
-	public Genom(Genom... gene )
-	{		
-		this.layers=gene[(int)(Math.random()*gene.length)].getLayers();
-		this.learningRate=gene[(int)(Math.random()*gene.length)].getLearningRate();
-		this.momentum=gene[(int)(Math.random()*gene.length)].getMomentum();
-		this.maxIterations=gene[(int)(Math.random()*gene.length)].getMaxIterations();
-		this.transferFunktion=gene[(int)(Math.random()*gene.length)].getTransferFunktion();
-		this.dataSet=gene[(int)(Math.random()*gene.length)].getDataSet();
+
+	public Genom(Genom gen1, Genom gen2) {
+		Genom[] gene = { gen1, gen2 };
+		java.util.Arrays.sort(gene);
+		int comfit = gene[0].getFitness() + gene[1].getFitness();
+		int maxfit = gene[0].getFitness();
+		this.layers = gene[Math.random() * comfit <= maxfit ? 0 : 1]
+				.getLayers();
+		this.learningRate = gene[Math.random() * comfit <= maxfit ? 0 : 1]
+				.getLearningRate();
+		this.momentum = gene[Math.random() * comfit <= maxfit ? 0 : 1]
+				.getMomentum();
+		this.maxIterations = gene[Math.random() * comfit <= maxfit ? 0 : 1]
+				.getMaxIterations();
+		this.transferFunktion = gene[Math.random() * comfit <= maxfit ? 0 : 1]
+				.getTransferFunktion();
+		this.dataSet = gene[Math.random() * comfit <= maxfit ? 0 : 1]
+				.getDataSet();
 	}
-	
-	private int[] rekombLayers(int[]... layer)
-	{
-		int durchSchnitt=0;
-		for(int i=0; i<layer.length;i++)
-			durchSchnitt+=layer[i].length;
-		durchSchnitt=(int) Math.round((double)durchSchnitt/layer.length);
-		int[] ret=new int[durchSchnitt];
-				//TODO Sinvoll mischen?
+
+	/*public Genom(Genom... gene) {
+		this.layers = gene[(int) (Math.random() * gene.length)].getLayers();
+		this.learningRate = gene[(int) (Math.random() * gene.length)]
+				.getLearningRate();
+		this.momentum = gene[(int) (Math.random() * gene.length)].getMomentum();
+		this.maxIterations = gene[(int) (Math.random() * gene.length)]
+				.getMaxIterations();
+		this.transferFunktion = gene[(int) (Math.random() * gene.length)]
+				.getTransferFunktion();
+		this.dataSet = gene[(int) (Math.random() * gene.length)].getDataSet();
+	}*/
+
+	private int[] rekombLayers(int[]... layer) {
+		int durchSchnitt = 0;
+		for (int i = 0; i < layer.length; i++)
+			durchSchnitt += layer[i].length;
+		durchSchnitt = (int) Math.round((double) durchSchnitt / layer.length);
+		int[] ret = new int[durchSchnitt];
+		// TODO Sinvoll mischen?
 		return ret;
 	}
-	
-	
-	public int[] getLayers()
-	{
+
+	public int[] getLayers() {
 		return layers;
 	}
 
-	public double getLearningRate()
-	{
+	public double getLearningRate() {
 		return learningRate;
 	}
 
-	public double getMomentum()
-	{
+	public double getMomentum() {
 		return momentum;
 	}
 
-	public int getMaxIterations()
-	{
+	public int getMaxIterations() {
 		return maxIterations;
 	}
 
-	public TransferFunctionType getTransferFunktion()
-	{
+	public TransferFunctionType getTransferFunktion() {
 		return transferFunktion;
 	}
 
-	public DataSet getDataSet()
-	{
+	public DataSet getDataSet() {
 		return dataSet;
 	}
-	
+
 	public int getFitness() {
 		return fitness;
 	}
 
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
 		Genom gen;
-		try
-		{
-			gen = new Genom(new int[]{100,64,36,16,4,2},0.001,0.25,Integer.MAX_VALUE,TransferFunctionType.TANH,TrainingSetImport
-					.importFromFile("spiel.log", 100, 2, ","));
-			System.out.println(new FitnessTester().test(gen,50000));
-		} catch (NumberFormatException e)
-		{
+		try {
+			gen = new Genom(new int[] { 100, 64, 36, 16, 4, 2 }, 0.001, 0.25,
+					Integer.MAX_VALUE, TransferFunctionType.TANH,
+					TrainingSetImport.importFromFile("spiel.log", 100, 2, ","));
+			System.out.println(new FitnessTester().test(gen, 50000));
+		} catch (NumberFormatException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} catch (FileNotFoundException e)
-		{
+		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} catch (IOException e)
-		{
+		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
-	
-	public void mutation()
-	{
-		if((int)(Math.random()+0.5)==0){
-			learningRate = Math.random()*2*learningRate + learningRate;
+
+	public void mutation() {
+		if ((int) (Math.random() + 0.1) == 0) {
+			if ((int) (Math.random() + 0.5) == 0) {
+				learningRate = Math.random() * 2 * learningRate + learningRate;
+				if (learningRate >= 0) {
+					learningRate = Math.random() * 0.7;
+				}
+			} else {
+				learningRate = Math.random() * 2 * learningRate - learningRate;
+				if (learningRate <= 0) {
+					learningRate = Math.random() * 0.01;
+				}
+			}
 		}
-		else{
-			learningRate = Math.random()*2*learningRate - learningRate;
-			if(learningRate<=0){
-				learningRate= Math.random()*0.01;
+		if ((int) (Math.random() + 0.1) == 0) {
+			if ((int) (Math.random() + 0.5) == 0) {
+				momentum = Math.random() * (momentum / 5) + momentum;
+				if (momentum >= 1) {
+					momentum = Math.random() * 0.7;
+				}
+			} else {
+				momentum = Math.random() * (momentum / 5) - momentum;
+				if (momentum <= 0) {
+					momentum = Math.random() * 0.3;
+				}
 			}
 		}
 		fitness = new FitnessTester().test(this, 5000);
@@ -126,10 +151,10 @@ public class Genom implements Comparable<Genom>
 
 	@Override
 	public int compareTo(Genom arg0) {
-		if(fitness < arg0.getFitness()){
+		if (fitness < arg0.getFitness()) {
 			return 1;
 		}
-		if(fitness > arg0.getFitness()){
+		if (fitness > arg0.getFitness()) {
 			return -1;
 		}
 		return 0;
