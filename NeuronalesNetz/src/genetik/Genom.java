30d3482b00b598951c56807855a8c5e5588305f8
jan <jan.hofmeier@yahoo.de>
2014-05-24 21:48:19 +0200

    Irgendwas

--- NeuronalesNetz/src/genetik/Genom.java
+++ NeuronalesNetz/src/genetik/Genom.java
@@ -7,7 +7,8 @@ import org.neuroph.core.data.DataSet;
 import org.neuroph.util.TrainingSetImport;
 import org.neuroph.util.TransferFunctionType;
 
-public class Genom implements Comparable<Genom> {
+public class Genom implements Comparable<Genom>
+{
 	/**
 	 * [0] muss zum feld passen und [length-1] muss 2 sein!
 	 */
@@ -21,7 +22,8 @@ public class Genom implements Comparable<Genom> {
 
 	public Genom(int[] layers, double learningRate, double momentum,
 			int maxIterations, TransferFunctionType transferFunktion,
-			DataSet trainingSet) {
+			DataSet trainingSet)
+	{
 		this.layers = layers;
 		this.learningRate = learningRate;
 		this.momentum = momentum;
@@ -30,7 +32,8 @@ public class Genom implements Comparable<Genom> {
 		this.dataSet = trainingSet;
 	}
 
-	public Genom(Genom gen1, Genom gen2) {
+	public Genom(Genom gen1, Genom gen2)
+	{
 		Genom[] gene = { gen1, gen2 };
 		java.util.Arrays.sort(gene);
 		int comfit = gene[0].getFitness() + gene[1].getFitness();
@@ -49,99 +52,88 @@ public class Genom implements Comparable<Genom> {
 				.getDataSet();
 	}
 
-	/*public Genom(Genom... gene) {
-		this.layers = gene[(int) (Math.random() * gene.length)].getLayers();
-		this.learningRate = gene[(int) (Math.random() * gene.length)]
-				.getLearningRate();
-		this.momentum = gene[(int) (Math.random() * gene.length)].getMomentum();
-		this.maxIterations = gene[(int) (Math.random() * gene.length)]
-				.getMaxIterations();
-		this.transferFunktion = gene[(int) (Math.random() * gene.length)]
-				.getTransferFunktion();
-		this.dataSet = gene[(int) (Math.random() * gene.length)].getDataSet();
-	}*/
+	/*
+	 * public Genom(Genom... gene) { this.layers = gene[(int) (Math.random() *
+	 * gene.length)].getLayers(); this.learningRate = gene[(int) (Math.random()
+	 * * gene.length)] .getLearningRate(); this.momentum = gene[(int)
+	 * (Math.random() * gene.length)].getMomentum(); this.maxIterations =
+	 * gene[(int) (Math.random() * gene.length)] .getMaxIterations();
+	 * this.transferFunktion = gene[(int) (Math.random() * gene.length)]
+	 * .getTransferFunktion(); this.dataSet = gene[(int) (Math.random() *
+	 * gene.length)].getDataSet(); }
+	 */
 
-	private int[] rekombLayers(int[]... layer) {
-		int durchSchnitt = 0;
-		for (int i = 0; i < layer.length; i++)
-			durchSchnitt += layer[i].length;
-		durchSchnitt = (int) Math.round((double) durchSchnitt / layer.length);
-		int[] ret = new int[durchSchnitt];
-		// TODO Sinvoll mischen?
-		return ret;
-	}
 
-	public int[] getLayers() {
+
+	public int[] getLayers()
+	{
 		return layers;
 	}
 
-	public double getLearningRate() {
+	public double getLearningRate()
+	{
 		return learningRate;
 	}
 
-	public double getMomentum() {
+	public double getMomentum()
+	{
 		return momentum;
 	}
 
-	public int getMaxIterations() {
+	public int getMaxIterations()
+	{
 		return maxIterations;
 	}
 
-	public TransferFunctionType getTransferFunktion() {
+	public TransferFunctionType getTransferFunktion()
+	{
 		return transferFunktion;
 	}
 
-	public DataSet getDataSet() {
+	public DataSet getDataSet()
+	{
 		return dataSet;
 	}
 
-	public int getFitness() {
+	public int getFitness()
+	{
 		return fitness;
 	}
 
-	public static void main(String[] args) {
-		Genom gen;
-		try {
-			gen = new Genom(new int[] { 100, 64, 36, 16, 4, 2 }, 0.001, 0.25,
-					Integer.MAX_VALUE, TransferFunctionType.TANH,
-					TrainingSetImport.importFromFile("spiel.log", 100, 2, ","));
-			System.out.println(new FitnessTester().test(gen, 50000));
-		} catch (NumberFormatException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-	}
-
-	public void mutation() {
-		if ((int) (Math.random() + 0.1) == 0) {
-			if ((int) (Math.random() + 0.5) == 0) {
+	public void mutation()
+	{
+		if ((int) (Math.random() + 0.1) == 0)
+		{
+			if ((int) (Math.random() + 0.5) == 0)
+			{
 				learningRate = Math.random() * 2 * learningRate + learningRate;
-				if (learningRate >= 0) {
+				if (learningRate >= 0)
+				{
 					learningRate = Math.random() * 0.7;
 				}
-			} else {
+			} else
+			{
 				learningRate = Math.random() * 2 * learningRate - learningRate;
-				if (learningRate <= 0) {
+				if (learningRate <= 0)
+				{
 					learningRate = Math.random() * 0.01;
 				}
 			}
 		}
-		if ((int) (Math.random() + 0.1) == 0) {
-			if ((int) (Math.random() + 0.5) == 0) {
+		if ((int) (Math.random() + 0.1) == 0)
+		{
+			if ((int) (Math.random() + 0.5) == 0)
+			{
 				momentum = Math.random() * (momentum / 5) + momentum;
-				if (momentum >= 1) {
+				if (momentum >= 1)
+				{
 					momentum = Math.random() * 0.7;
 				}
-			} else {
+			} else
+			{
 				momentum = Math.random() * (momentum / 5) - momentum;
-				if (momentum <= 0) {
+				if (momentum <= 0)
+				{
 					momentum = Math.random() * 0.3;
 				}
 			}
@@ -149,11 +141,14 @@ public class Genom implements Comparable<Genom> {
 	}
 
 	@Override
-	public int compareTo(Genom arg0) {
-		if (fitness < arg0.getFitness()) {
+	public int compareTo(Genom arg0)
+	{
+		if (fitness < arg0.getFitness())
+		{
 			return 1;
 		}
-		if (fitness > arg0.getFitness()) {
+		if (fitness > arg0.getFitness())
+		{
 			return -1;
 		}
 		return 0;
@@ -161,6 +156,31 @@ public class Genom implements Comparable<Genom> {
 
 	void setFitness(int fitness)
 	{
-		this.fitness=fitness;
+		this.fitness = fitness;
+	}
+
+	public static void main(String[] args)
+	{
+		Genom gen;
+		try
+		{
+			gen = new Genom(new int[] { 100, 64, 36, 16, 4, 2 }, 0.001, 0.25,
+					Integer.MAX_VALUE, TransferFunctionType.TANH,
+					TrainingSetImport.importFromFile("spiel.log", 100, 2, ","));
+			System.out
+					.println(new FitnessTester().test(gen, Integer.MAX_VALUE));
+		} catch (NumberFormatException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (FileNotFoundException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
