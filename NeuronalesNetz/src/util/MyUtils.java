3917cf0ccfaad3efb208b721cc2b2c40bc784f9b
Richard Polzer <richard@polzer-eb.de>
2014-05-29 18:44:24 +0200

    speichern

--- NeuronalesNetz/src/util/MyUtils.java
+++ NeuronalesNetz/src/util/MyUtils.java
@@ -1,28 +1,27 @@
 package util;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class MyUtils
-{
-	public static String readFile(File f)
-	{
+public class MyUtils {
+	public static String readFile(File f) {
 		return readFile(f.getAbsoluteFile());
 	}
-	
-	public static String readFile(String name) throws IOException
-	{
-		BufferedReader bR = new BufferedReader(new InputStreamReader(new FileInputStream(name), "UTF-8"));
+
+	public static String readFile(String name) throws IOException {
+		BufferedReader bR = new BufferedReader(new InputStreamReader(
+				new FileInputStream(name), "UTF-8"));
 		StringBuffer inhalt = new StringBuffer();
 		String line = bR.readLine();
-		while (true)
-		{
+		while (true) {
 			inhalt.append(line);
 			line = bR.readLine();
-			if(line==null)
+			if (line == null)
 				break;
 			inhalt.append('\n');
 		}
@@ -30,10 +29,19 @@ public class MyUtils
 		return inhalt.toString();
 	}
 
-	public static void writeFile(String string, String string2)
-	{
-		// TODO Auto-generated method stub
-		
+	public static void writeFile(String inhalt, String name) throws IOException {
+		writeFile(inhalt, name, false);
 	}
-	
+
+	public static void writeFile(String inhalt, String name, boolean append)
+			throws IOException {
+		writeFile(inhalt, new File(name), append);
+	}
+
+	public static void writeFile(String inhalt, File f, boolean append)
+			throws IOException {
+		BufferedWriter bW = new BufferedWriter(new FileWriter(f, append));
+		bW.write(inhalt);
+		bW.close();
+	}
 }
