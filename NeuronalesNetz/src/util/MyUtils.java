41dab52d3bf3310ce54f40efc98f39c7a7205d19
jan <jan.hofmeier@yahoo.de>
2014-05-29 21:50:43 +0200

    MyUtils kommentiert

--- NeuronalesNetz/src/util/MyUtils.java
+++ NeuronalesNetz/src/util/MyUtils.java
@@ -8,17 +8,36 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class MyUtils {
-	public static String readFile(File f) {
+public class MyUtils
+{
+
+	/**
+	 * Liest den Kompletten Inhalt des Files
+	 * 
+	 * @param f
+	 *            File welches gelesen werden soll
+	 * @return Inhalt der Datei
+	 */
+	public static String readFile(File f) throws IOException
+	{
 		return readFile(f.getAbsoluteFile());
 	}
 
-	public static String readFile(String name) throws IOException {
+	/**
+	 * Liest den kompletten Inhalt des Files
+	 * 
+	 * @param name Name der Datei mit Pfad
+	 * @return Inhalt der Datei
+	 * @throws IOException
+	 */
+	public static String readFile(String name) throws IOException
+	{
 		BufferedReader bR = new BufferedReader(new InputStreamReader(
 				new FileInputStream(name), "UTF-8"));
 		StringBuffer inhalt = new StringBuffer();
 		String line = bR.readLine();
-		while (true) {
+		while (true)
+		{
 			inhalt.append(line);
 			line = bR.readLine();
 			if (line == null)
@@ -29,17 +48,40 @@ public class MyUtils {
 		return inhalt.toString();
 	}
 
-	public static void writeFile(String inhalt, String name) throws IOException {
+	/**
+	 * Schreibt den String in eine Datei, fall vorhanden wird die Datei ueberschrieben
+	 * @param inhalt String der in dei Datei geschrieben werden soll
+	 * @param name Pfad zur Datei
+	 * @throws IOException
+	 */
+	public static void writeFile(String inhalt, String name) throws IOException
+	{
 		writeFile(inhalt, name, false);
 	}
 
+	/**
+	 * Schreibt den String in eine Datei
+	 * @param inhalt String der in dei Datei geschrieben werden soll
+	 * @param name Pfad zur Datei
+	 * @param append soll der inhalt hinten an die Datein angehaengt werden, fall schon vorhanden
+	 * @throws IOException
+	 */
 	public static void writeFile(String inhalt, String name, boolean append)
-			throws IOException {
+			throws IOException
+	{
 		writeFile(inhalt, new File(name), append);
 	}
 
+	/**
+	 * Schreibt den String in eine Datei
+	 * @param inhalt String der in dei Datei geschrieben werden soll
+	 * @param f Datei in die geschrieben werden soll
+	 * @param append soll der inhalt hinten an die Datein angehaengt werden, fall schon vorhanden
+	 * @throws IOException
+	 */
 	public static void writeFile(String inhalt, File f, boolean append)
-			throws IOException {
+			throws IOException
+	{
 		BufferedWriter bW = new BufferedWriter(new FileWriter(f, append));
 		bW.write(inhalt);
 		bW.close();
