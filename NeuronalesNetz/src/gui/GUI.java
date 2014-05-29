f992fcb6cd90fc800a7e0aef55316282c18951c6
Richard Polzer <richard@polzer-eb.de>
2014-05-29 19:43:24 +0200

    Gui

--- NeuronalesNetz/src/gui/GUI.java
+++ NeuronalesNetz/src/gui/GUI.java
@@ -86,7 +86,7 @@ public class GUI extends JFrame {
 	public GUI() {
 		setMinimumSize(new Dimension(350, 575));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 292, 575);
+		setBounds(100, 100, 304, 575);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
